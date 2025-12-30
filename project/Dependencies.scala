a94a4953a84a9e215f51aa26180f87babcb8451a
sayochika <sayochika@gmail.com>
2025-12-30 12:38:41 +0000

    Testing the new version of ui-test-runner

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
 //    "uk.gov.hmrc" %% "ui-test-runner" % "0.51.0" % Test
-    "uk.gov.hmrc" %% "ui-test-runner-platui-3896" % "0.5.0-SNAPSHOT" % Test
+    "uk.gov.hmrc" %% "ui-test-runner" % "0.53.0-SNAPSHOT" % Test
   )
 
 }
