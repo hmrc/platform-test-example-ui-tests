4746cef8a0da68ddd0024586a90a14ca108ed864
sayochika <sayochika@gmail.com>
2026-01-06 12:05:12 +0000

    Added new version of ui-test-runner

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val test: Seq[ModuleID] = Seq(
 //    "uk.gov.hmrc" %% "ui-test-runner" % "0.51.0" % Test
 //    "uk.gov.hmrc" %% "ui-test-runner" % "0.53.0-SNAPSHOT" % Test
-    "uk.gov.hmrc" %% "ui-test-runner-platui-3896" % "0.7.0" % Test
+    "uk.gov.hmrc" %% "ui-test-runner-platui-3896" % "0.8.0" % Test
   )
 
 }
